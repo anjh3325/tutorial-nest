ddab26d7f9ac1c55b76493edc753a25c1c0d8c95
Anjh <anjh3325@gmail.com>
2023-09-12 18:54:02 +0900

    install로 설치하고 authModule에 기본 값 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
